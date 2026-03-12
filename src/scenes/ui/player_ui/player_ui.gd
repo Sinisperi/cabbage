f554f2ec6f68a0157edf9c32c4bd85c2fa392956
Sinisperi <140267285+Sinisperi@users.noreply.github.com>
2026-03-12 10:52:41 +0200

    update player ui on ready

--- src/scenes/ui/player_ui/player_ui.gd
+++ src/scenes/ui/player_ui/player_ui.gd
@@ -18,6 +18,7 @@ var ui_state: int = HUD
 func _ready() -> void:
 	Globals.player_ui = self
 	EventBus.ui.character_cretion_finished.connect(_on_character_creation_finished)
+	_update_ui()
 
 func _unhandled_input(event: InputEvent) -> void:
 	if event.is_action_pressed("toggle_inventory"):
